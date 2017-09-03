ef4ad11d2ca5a54ade0052f751d56a10b217e665
Markus Hederström <d0namiz@gmail.com>
2017-09-03 20:44:30 +0200

    added message component

---
+++ src/components/core/components/message.vue
@@ -0,0 +1,18 @@
+<template>
+
+  <div class="message" v-if="message">
+    <h3>{{ message.title }}</h3>
+    <p>{{ message.body }}</p>
+  </div>
+
+</template>
+
+<script>
+export default {
+  props: ['message'],
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
